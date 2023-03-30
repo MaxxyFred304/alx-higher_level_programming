35b83dbd86209cacdbcc8beeec10972a1ca4076f
MaxxyFred304 <maxfred304@gmail.com>
2023-03-30 05:55:35 -0700

    script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a given URL and display the response status code.
-curl -X POST -H "Content-Type: application/json" --data-binary "@${2}" "${1}"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
